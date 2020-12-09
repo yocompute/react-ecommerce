26e2b4063ae2e6785369ec98375ee18781755c2f
zlkca <likzhang@gmail.com>
2020-12-09 00:04:24 -0500

    update payment page

--- src/pages/payment/PaymentPage.js
+++ src/pages/payment/PaymentPage.js
@@ -1,17 +1,26 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux'
-import PropTypes from "prop-types"
+import { connect } from 'react-redux';
+import PropTypes from "prop-types";
+import { makeStyles } from '@material-ui/core/styles';
 
-import Button from '@material-ui/core/Button'
+
+// import Button from '@material-ui/core/Button'
 import { CartItemList } from '../../components/cart/CartItemList';
-import { PaymentMethodSelect } from '../../components/common/PaymentMethodSelect'
+// import { PaymentMethodSelect } from '../../components/common/PaymentMethodSelect'
 import { setPage } from  '../../redux/page/page.actions';
 import { PAYMENT_PAGE } from '../../const';
 
 // import Header from '../../components/common/Header'
 
+const useStyles = makeStyles( theme => ({
+    page: {
+        padding: '20px'
+    }
+}));
+
 const PaymentPage = ({cart, setPage}) => {
+    const classes = useStyles();
     const handlePaymentMethodSelect = () => {
 
     }
@@ -21,10 +30,10 @@ const PaymentPage = ({cart, setPage}) => {
     }, [setPage])
 
     return (
-        <div>
+        <div className={classes.page}>
             {/* <Header title={'Order Page'}></Header> */}
             <CartItemList items={cart.items}/>
-            <div className="label payment-label">Payment Method</div>
+            {/* <div className="label payment-label">Payment Method</div> */}
             {/* <PaymentMethodSelect onSelect={handlePaymentMethodSelect}></PaymentMethodSelect> */}
         </div>
     )
